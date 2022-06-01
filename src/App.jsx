f9df5a46a1e7f5f281624ab6d755f7c47e9acaeb
NotPraneeth <rsphaha@gmail.com>
2022-06-01 23:21:36 +0530

    completed file structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Topbar from "./components/topbar/Topbar";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar/>
+    </div>
+  );
+}
+
+export default App;
