d6108797268e4fc7857258a0bea35822487865ec
NotPraneeth <rsphaha@gmail.com>
2022-06-03 00:06:30 +0530

    worked on the hamburger menu

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import { useState } from "react";
+import Menu from './components/menu/Menu';
 
 
 
@@ -16,6 +17,7 @@ function App() {
   return (
     <div className="app">
       <Topbar menuOpen = { menuOpen } setMenuOpen = { setMenuOpen }/>
+      <Menu menuOpen = { menuOpen } setMenuOpen = { setMenuOpen }/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
