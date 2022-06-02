29cfc3ebbc599b0ee8639bdb895e45dd20199564
NotPraneeth <rsphaha@gmail.com>
2022-06-03 03:52:59 +0530

    finished the intro component

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
-import "./intro.scss"
+import "./intro.scss";
 
-export default function intro() {
+
+
+
+export default function Intro() {
   return (
-    <div className='intro' id='intro'></div>
+    <div className='intro' id='intro'>
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/casual-life-3d-boy-sitting-at-the-desk-with-open-book.png" alt="" className="introImg" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hello there, I'm</h2>
+          <h1>Sai Praneeth</h1>
+          <h3>And I Develop websites.</h3>
+        </div>
+        <a href='#portfolio'>
+          <img src="assets/down.png" alt=""/>
+        </a>
+      </div>
+    </div>
   )
 }
