c9b6ba68080f176628f37285f70a85c862437c6b
NotPraneeth <rsphaha@gmail.com>
2022-06-02 20:35:41 +0530

    enabled scroll snapping and worked on overall layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
+import React from 'react';
+import "./app.scss"
 import Topbar from "./components/topbar/Topbar";
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
 import Works from "./components/works/Works";
 import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
+import { useState } from "react";
+import ReactDOM from 'react-dom';
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+
+
 
 function App() {
+  const [menuOpen,setMenuOpen] = useState(false)
   return (
     <div className="app">
-      <Topbar/>
+      <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}/>
       <div className="sections">
         <Intro/>
         <Portfolio/>
         <Works/>
         <Testimonials/>
         <Contact/>
-      </div>
+        </div>
     </div>
 
   );
